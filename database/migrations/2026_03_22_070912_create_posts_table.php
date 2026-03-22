7d5c47274ec71a98ecb8950e942fb8a19fbd792a
ahmad-suliman <ahmadalisuleiman8@gmail.com>
2026-03-22 16:40:32 +0300

    fix migration row

--- database/migrations/2026_03_22_070912_create_posts_table.php
+++ database/migrations/2026_03_22_070912_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('username');
             $table->string('title');
             $table->text('message');
             $table->timestamps();
